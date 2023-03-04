0d91c3317daacc1194fb29dba5f2aa6f597ad3cc
Mitterrand <105891246+mitterrand-monteiro@users.noreply.github.com>
2023-03-04 20:15:59 -0300

    Create scroll stop event handler

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useScrollIntoView } from '@mantine/hooks';
 import { Paper } from '@mantine/core';
 import { FixedHeader } from '../components/Header/Header';
@@ -5,6 +6,7 @@ import { Home } from '../components/Home/Home';
 import { About } from '../components/About/About';
 import { Experience } from '../components/Experience/Experience';
 import { Contact } from '../components/Contact/Contact';
+import { HEADER_HEIGHT } from '../components/Header/Header.styles';
 
 export default function HomePage() {
   const { scrollIntoView: scrollIntoHome, targetRef: homeRef } =
@@ -15,6 +17,48 @@ export default function HomePage() {
     useScrollIntoView<HTMLDivElement>();
   const { scrollIntoView: scrollIntoContact, targetRef: contactRef } =
     useScrollIntoView<HTMLDivElement>();
+
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [active, setActive] = useState(0);
+
+  const handleScrollStop = () => {
+    const position = window.pageYOffset - HEADER_HEIGHT;
+
+    if (position <= homeRef.current.offsetTop) {
+      setActive(0);
+    } else if (position <= aboutRef.current.offsetTop) {
+      setActive(1);
+    } else if (position <= experienceRef.current.offsetTop) {
+      setActive(2);
+    } else {
+      setActive(3);
+    }
+  };
+
+  useEffect(() => {
+    let timeout: number | null = null;
+
+    const handleScroll = () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+
+      setIsScrolling(true);
+      timeout = window.setTimeout(() => {
+        setIsScrolling(false);
+        handleScrollStop();
+      }, 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, []);
 
   return (
     <>
@@ -29,6 +73,7 @@ export default function HomePage() {
           { label: 'Experience', link: 'experience' },
           { label: 'Contact', link: 'contact' },
         ]}
+        activeLink={active}
       />
       <Paper ref={homeRef}>
         <Home scrollAbout={scrollIntoAbout} />
