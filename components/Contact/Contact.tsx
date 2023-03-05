a7b3effaaa1c2044b19b22550a41313619d37da2
Mitterrand <105891246+mitterrand-monteiro@users.noreply.github.com>
2023-03-05 16:40:30 -0300

    Format Contact componente

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,23 +1,33 @@
 import { Title, Text, ActionIcon, Group } from '@mantine/core';
 import useStyles from './Contact.styles';
-import { IconBrandLinkedin, IconBrandGithub  } from '@tabler/icons';
+import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons';
 
 export function Contact() {
   const { classes } = useStyles();
 
   return (
     <>
-      <Title className={classes.title} align="center" mt={100}>
+      <Title className={classes.title} align="center">
         <Text inherit variant="gradient" component="span">
           Contact
         </Text>
       </Title>
       <Group position="center">
-        <ActionIcon size={80} target="_blank" component="a" href="https://www.linkedin.com/in/mitterrandmonteiro/">
-          <IconBrandLinkedin size="80"/>
+        <ActionIcon
+          size={80}
+          target="_blank"
+          component="a"
+          href="https://www.linkedin.com/in/mitterrandmonteiro/"
+        >
+          <IconBrandLinkedin size="80" />
         </ActionIcon>
-        <ActionIcon size={80} target="_blank" component="a" href="https://github.com/mitterrand-monteiro/">
-          <IconBrandGithub size="80"/>
+        <ActionIcon
+          size={80}
+          target="_blank"
+          component="a"
+          href="https://github.com/mitterrand-monteiro/"
+        >
+          <IconBrandGithub size="80" />
         </ActionIcon>
       </Group>
     </>
